f1d66154a2b94d3cbed54ab8ee1ea5a980d11b86
Sure-Mohan <151706255+Sure-Mohan@users.noreply.github.com>
2024-11-05 15:13:14 +0530

    StringDemo1.java

---
+++ StringDemo1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.String;
+public class StringDemo1{
+    public static void main(String args[]){
+        String s1="welcome";
+        char[]ch={'M','o','h','a','n'};
+        String s2=new String(ch);
+        String s3=new String("Example");
+        System.out.println("String s1 is "+s1);
+         System.out.println("String s2 is "+s2);
+          System.out.println("String s3 is "+s3);
+    }
+}
