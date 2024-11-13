cc397df8431c16e263927edb8fa07d894457f9c3
Sure-Mohan <151706255+Sure-Mohan@users.noreply.github.com>
2024-11-13 19:28:39 +0530

    DatatypesDemo.java

---
+++ DatatypesDemo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class DatatypesDemo{
+    public static void main(String args[]){
+         boolean booleanvar=true;
+        char charvar='A';
+        byte bytevar=100;
+        short shortvar=10000;
+        int intvar=100000;
+        long longvar=1000000;
+        double doublevar=19.99;
+        float floatvar=5.75f;
+        System.out.println("boolean:"+booleanvar);
+        System.out.println("char:"+charvar);
+        System.out.println("byte:"+bytevar);
+        System.out.println("short:"+shortvar);
+        System.out.println("int:"+intvar);
+        System.out.println("long:"+longvar);
+        System.out.println("double:"+doublevar);
+        System.out.println("float:"+floatvar);
+
+
+    }
+}
